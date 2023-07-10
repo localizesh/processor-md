53496e4a047ee6f6895dfc7338c88c9c499b815e
SiarheiBalt <jh-11@yandex.by>
2023-07-10 10:44:53 +0300

    Add html syntax for lists

--- src/handlers/hast-to-mdast/handlers.ts
+++ src/handlers/hast-to-mdast/handlers.ts
@@ -1,5 +1,6 @@
 import {all as toMdastAll} from "rehype-remark";
 import { Element } from "hast";
+import {toHtml} from 'hast-util-to-html';
 
 export enum ListTypes {
   ol = "ol",
@@ -8,6 +9,12 @@ export enum ListTypes {
 
 export const listToMdast = (h: any, node: Element, type: ListTypes) => {
   const spread: boolean = node.properties?.spread === "true";
+  const isNodeSyntaxHtml: boolean = !node.properties?.marker;
+
+  if(isNodeSyntaxHtml) {
+    const res: string = toHtml(node);
+    return {type: "html", value: res};
+  }
 
   let element: any = {
     start: node.properties?.start,

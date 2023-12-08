79d8f81db9e292ea97bb4d32714c8878fa76a154
SiarheiBalt <jh-11@yandex.by>
2023-12-08 11:10:11 +0300

    Fix structure parsing, iframe tag parsing

--- test/processor.spec.ts
+++ test/processor.spec.ts
@@ -23,6 +23,7 @@ function processAndCompare(filename: string) {
 
 describe('MdProcessorTest', function() {
   it('documents should be equal', function() {
+    processAndCompare('problem.md');
     processAndCompare('md-html-complex.md');
     processAndCompare('simple-test.md');
     processAndCompare('link.md');

041a3de39ce537ed87aabfac687b8c9eac0cb28d
artsevruk <artsevruk@gmail.com>
2024-03-13 16:12:23 +0300

    Fix link in html tags

--- test/processor.spec.ts
+++ test/processor.spec.ts
@@ -24,6 +24,7 @@ function processAndCompare(filename: string) {
 describe('MdProcessorTest', function() {
   it('documents should be equal', function() {
     processAndCompare('md-html-complex.md');
+    processAndCompare('html-links.md');
     processAndCompare('simple-test.md');
     processAndCompare('link.md');
     processAndCompare('headings.md');

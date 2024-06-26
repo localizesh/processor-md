6719f428a864ab41771eba08deb03e30f8d5d650
SiarheiBalt <75533283+SiarheiBalt@users.noreply.github.com>
2024-06-26 10:53:51 +0300

    Replace lib with SDK (#165)

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,9 @@
-import {Segment} from "@localizeio/lib";
+import {Segment, LayoutElement, Tags} from "@localizesh/sdk";
 
 export type SegmentsMap = {
   [id: string]: Segment;
+};
+
+export interface LayoutElementWithTags extends LayoutElement {
+  tags?: Tags
 };
